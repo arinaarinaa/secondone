026c6bde8fcc65054660a1da7756bdffb159cd2b
arinach <arina.tchukurova@yandex.ru>
2022-08-19 23:41:57 +0300

    dd nachnem

---
+++ nachnem/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+//(делится ли оно на два без остатка)
+
+int x;
+Console.WriteLine("Введи тут число - ");
+x = Convert.ToInt32(Console.ReadLine());
+
+if(x % 2==0)
+{
+    Console.WriteLine("Yep, bro");
+}
+else
+{
+    Console.WriteLine("Nooope, really sory");
+};
